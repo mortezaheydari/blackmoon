eb332a4bb26d6b608b14312aab316c064e35aa44
shahin70000 <shahin70000@gmail.com>
2012-12-25 18:25:12 +0300

    priliminary "edit user profile" developed
    ...also
    -Some views generated for User
    -gravatar_for(user) method added to user_helper
    -debug(params) added to layout
    _error_messages partial added to views\shared folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :profile_attributes
   validates :name, presence: true, length: {maximum: 50}, uniqueness: true
 
   has_one :profile
+  accepts_nested_attributes_for :profile  
   belongs_to :account
 end
