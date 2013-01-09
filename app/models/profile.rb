e4e8af37f75af352e0139ce0073a6cb38345e325
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-01-09 13:47:41 +0300

    added profile about validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,8 @@
 class Profile < ActiveRecord::Base
   attr_accessible :about, :date_of_birth, :first_name, :gender, :last_name, :phone
 
+  validates_length_of :about, within: 10..120, on: :update, message: "must be between 10 to 120 charachters."
+
+
 	belongs_to :user
 end
