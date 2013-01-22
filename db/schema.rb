9a5288bc23b152a4c1e8c89faf7866a375b5ee8f
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-01-22 16:03:02 +0300

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122083022) do
+ActiveRecord::Schema.define(:version => 20130122121600) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(:version => 20130122083022) do
     t.string   "offering_type"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "offering_participations", :force => true do |t|
+    t.integer  "participator_id"
+    t.integer  "offering_id"
+    t.string   "offering_type"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "profiles", :force => true do |t|
