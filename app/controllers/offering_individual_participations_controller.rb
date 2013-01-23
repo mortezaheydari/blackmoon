245213d2c1578afadc2015bdb06ab9a75df98c94
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-01-23 11:14:50 +0300

    Join Fixed

--- app/controllers/offering_individual_participations_controller.rb
+++ app/controllers/offering_individual_participations_controller.rb
@@ -1,17 +1,24 @@
 class OfferingIndividualParticipationsController < ApplicationController
+            include SessionsHelper
 
 	before_filter :authenticate_account!
 
+            def index
+
+            end
+
 	def create
-		@offering_type = params[offering_type]
-		@offering_id = params[offering_id]
-		@user = params[joining_user]
-		if @offering_type = "event"		
+                     @offering = params[:event]
+		@offering_type = params[:offering_type]
+		@offering_id = params[:offering_id].to_i
+		@user = User.find_by_id(params[:joining_user])
+		if @offering_type = "event"
 			@user.events_participating << Event.find_by_id(@offering_id)
+                                 redirect_to root_path
 		end
 		# respond_to do |format|
 		# 	format.html { redirect_to @user }
-		# 	format.js
+			# format.js
 		# end
 	end
 
