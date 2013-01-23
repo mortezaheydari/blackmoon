9bdee096ae8f3c5954aab9e88f36f75a5d02f6e5
shahin70000 <shahin70000@gmail.com>
2013-01-23 14:11:21 +0300

    join controler method coded

--- app/controllers/offering_individual_participations_controller.rb
+++ app/controllers/offering_individual_participations_controller.rb
@@ -8,31 +8,50 @@ class OfferingIndividualParticipationsController < ApplicationController
             end
 
 	def create
-                     @offering = params[:event]
-		@offering_type = params[:offering_type]
-		@offering_id = params[:offering_id].to_i
-		@user = User.find_by_id(params[:joining_user])
-		if @offering_type = "event"
-			@user.events_participating << Event.find_by_id(@offering_id)
-                                 redirect_to root_path
+
+    offering = params[:offering_type]
+		user = User.find_by_id(params[:joining_user])
+		offering_id = params[:offering_id]
+
+		if user.respond_to? "#{offering}s_participating" and ["event","class","game"].include? offering
+
+			offerings_participating = user.send("#{offering}s_participating")
+			joining_offering = offering.camelize.constantize.find_by_id(offering_id)
+			number_of_attendings = joining_offering.number_of_attendings
+
+			if offerings_participating.count < number_of_attendings or number_of_attendings = 0
+		  	offerings_participating << joining_offering
+		  end
+			respond_to do |format|
+				format.html { redirect_to joining_offering }
+				format.js
+			end
+		else
+			redirect_to @user
 		end
-		# respond_to do |format|
-		# 	format.html { redirect_to @user }
-			# format.js
-		# end
 	end
 
-	def destroy
-		@offering_type = params[offering_type]
-		@offering_id = params[offering_id]
-		@user = params[leaving_user]
-		@offering = @user.offering_individual_participations.where(offering_type: @offering_type, offering_id: @offering_id)
-		@offering.destroy unless @offering == []
+	# def destroy
+	# 	@offering_type = params[offering_type]
+	# 	@offering_id = params[offering_id]
+	# 	@user = params[leaving_user]
+	# 	@offering = @user.offering_individual_participations.where(offering_type: @offering_type, offering_id: @offering_id)
+	# 	@offering.destroy unless @offering == []
 
-		# respond_to do |format|
-		# 	format.html { redirect_to @user }
-		# 	format.js
-		# end
-	end
+	# 	# respond_to do |format|
+	# 	# 	format.html { redirect_to @user }
+	# 	# 	format.js
+	# 	# end
+	# end
+
+	# private
+	# 	def get_offering(offering_type)
+	# 		params[:event] if offering_type = "event"		
+	# 	end
+
+	# 	def join_event
+	# 		if events_participating.count < number_of_attendings or number_of_attendings = 0
+	# 		events_participating << Event.find_by_id(@offering_id)
+
 
 end
