e6fa9126cee3ebcba06a0622e8939986a5be020c
shahin70000 <shahin70000@gmail.com>
2013-01-27 18:31:57 +0300

    [X]OfferingIndividualParticipationsController.create
    [X]OfferingIndividualParticipationsController.destroy
    
    private def name_is_valid?(user, name)

--- app/controllers/offering_individual_participations_controller.rb
+++ app/controllers/offering_individual_participations_controller.rb
@@ -1,56 +1,66 @@
+# controller responsible for joining buttons
 class OfferingIndividualParticipationsController < ApplicationController
-            include SessionsHelper
+  include SessionsHelper
 
 	before_filter :authenticate_account!
 
-            def index
+  def index
+    # temp fix
+  end
 
-            end
-
+  # joining an offering
   def create
       offering = params[:offering_type]
       user = User.find_by_id(params[:joining_user])
       offering_id = params[:offering_id]
 
-      if user.respond_to? "#{offering}s_participating" and ["event","class","game"].include? offering
+      if name_is_valid?(user, offering)
 
-          offerings_participating = user.send("#{offering}s_participating")
-          joining_offering = offering.camelize.constantize.find_by_id(offering_id)
-          number_of_attendings = joining_offering.number_of_attendings
-          if offerings_participating.count < number_of_attendings or number_of_attendings = 0
-              offerings_participating << joining_offering
-          end
-          @participator = joining_offering.individual_participators
-          respond_to do |format|
-              format.html { redirect_to joining_offering }
-              format.js
-          end
+        offerings_participating = user.send("#{offering}s_participating")
+        joining_offering = offering.camelize.constantize.find_by_id(offering_id)
+        number_of_attendings = joining_offering.number_of_attendings
+        if offerings_participating.count < number_of_attendings or number_of_attendings = 0
+            # todo: check participation deadline is not pass
+            offerings_participating << joining_offering
+        end
+        @participator = joining_offering.individual_participators
+        respond_to do |format|
+            format.html { redirect_to joining_offering }
+            format.js
+        end
       else
           redirect_to @user
       end
   end
 
-	# def destroy
-	# 	@offering_type = params[offering_type]
-	# 	@offering_id = params[offering_id]
-	# 	@user = params[leaving_user]
-	# 	@offering = @user.offering_individual_participations.where(offering_type: @offering_type, offering_id: @offering_id)
-	# 	@offering.destroy unless @offering == []
+  # leaving an offering
+  def destroy
+      offering = params[:offering_type]
+      user = User.find_by_id(params[:leaving_user])
+      offering_id = params[:offering_id]
 
-	# 	# respond_to do |format|
-	# 	# 	format.html { redirect_to @user }
-	# 	# 	format.js
-	# 	# end
-	# end
+      if name_is_valid?(user, offering)
+      participations = user.offering_individual_participations.where(offering_type: offering, offering_id: offerign_id)
+      # todo: check participation deadline is not pass
+      participations.each.destroy unless participations == []
 
-	# private
-	# 	def get_offering(offering_type)
-	# 		params[:event] if offering_type = "event"		
-	# 	end
+        leaving_offering = offering.camelize.constantize.find_by_id(offering_id)
+        @participator = leaving_offering.individual_participators
+        respond_to do |format|
+            format.html { redirect_to leaving_offering }
+            format.js
+        end
+      else
+          redirect_to @user
+      end    
+  end
 
-	# 	def join_event
-	# 		if events_participating.count < number_of_attendings or number_of_attendings = 0
-	# 		events_participating << Event.find_by_id(@offering_id)
+  private
 
+    # checks if offerign name is valid for user
+    # note: this function is controller specific
+    def name_is_valid?(user, name)
+      user.respond_to? "#{name}s_participating" and ["event","class","game"].include? name
+    end
 
 end
