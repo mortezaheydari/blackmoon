157d4f34c55fedb46b37a972ffbdd02ac793a613
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-02-06 18:11:54 +0300

    Revert "Follow and unfollow files added"
    This reverts commit 7e1f39877e0ebee8053ea7e9d7222e8dbc96015c.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,10 @@
 module EventsHelper
-    def toggle_like_button(event, user)
-        if user.flagged?(event, :like)
-            link_to "Unlike", like_event_path(event)
-        else
-            link_to "like", like_event_path(event)
-        end
-    end
+
+	toggle_like_button(event, user)
+		if user.flagged?(event, :like)
+			link_to "Unlike", like_event_path(event)
+		else
+			link_to "like", like_event_path(event)
+		end
+	end
 end
