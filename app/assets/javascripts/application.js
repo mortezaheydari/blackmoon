3a09c846b6e956b532f86039baaa4c0b4b0770d6
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-02-07 15:28:48 +0300

    event Lobby Like Fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,10 @@
 //= require jquery_ujs
 //= require_tree .
 
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
+
 $(document).ready(function() {
     var $modal = $('#modal');
     var $modal_close = $modal.find('.close');
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,9 +10,22 @@ module EventsHelper
 
             def toggle_like_card_button(event, user)
                 if user.flagged?(event, :like)
-                    link_to content_tag("div", "", class: "thumbedUp"), like_event_path(event), card_id: event.id
+                    link_to content_tag("div", "", class: "thumbedUp"), like_cards_event_path(event), id: event.id, remote: true
                 else
-                    ink_to content_tag("div", "", class: "thumbsUp"), like_event_path(event), card_id: event.id
+                    link_to content_tag("div", "", class: "thumbsUp"), like_cards_event_path(event), id: event.id, remote: true
                 end
             end
+
+            # def toggle_like_card_button(event, user)
+            #     if user.flagged?(event, :like)
+            #         @class = "thumbedUp"
+            #     else
+            #         @class = "thumbsUp"
+            #     end
+            #     form_for(event, remote: true, url: like_cards_event_path(event)) do |f|
+            #         f.hidden_field :id
+            #         f.submit "hello"
+            #     end
+            # end
+
 end
