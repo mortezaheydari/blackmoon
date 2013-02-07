a5ae1a8ec0ef581e0dccf0e6de57a0d3972ee866
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-02-07 22:59:35 +0300

    Commented precompile application.rb

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Blackmoon
     config.active_support.escape_html_entities_in_json = true
 
     # Added for heroku
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
