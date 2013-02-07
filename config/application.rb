a302a14b5ba96f2d08e53e07176c539000ed7534
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-02-07 23:23:39 +0300

    changed the location

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ end
 
 module Blackmoon
   class Application < Rails::Application
+
+     # Added for heroku
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -41,9 +45,6 @@ module Blackmoon
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
-
-    # Added for heroku
-    # config.assets.initialize_on_precompile = false
 
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
