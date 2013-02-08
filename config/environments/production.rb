941a24e47536d5e81bf4244a27fccbc43598ab75
Shahin Hojabrian <shahin70000@gmail.com>
2013-02-08 03:51:43 -0800

    preparing for precompile
    config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,14 @@ Blackmoon::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
+  # ssh: this can be used in case of running application offline in froduction mode and facing problem with sstatic asset serve.
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
