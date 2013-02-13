63a13392f6b561f2553e2fa9c74a29afb2c17e6d
shahin70000 <shahin70000@gmail.com>
2013-02-13 09:36:48 +0300

    mistake fixed in
    act_creation and offering_creation mixed-up

--- app/models/act_creation.rb
+++ app/models/act_creation.rb
@@ -1,5 +1,5 @@
-class ActCreation < ActiveRecord::Base
-  attr_accessible :creator_id, :offering_id, :offering_type
+class ActCreation < ActiveRecord::Base 
+	attr_accessible :creator_id, :act_id, :act_type  
 
   belongs_to :act, polymorphic: true
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
--- app/models/offering_creation.rb
+++ app/models/offering_creation.rb
@@ -1,6 +1,6 @@
 class OfferingCreation < ActiveRecord::Base
-  attr_accessible :creator_id, :act_id, :act_type
+  attr_accessible :creator_id, :offering_id, :offering_type  
 
-  belongs_to :act, polymorphic: true
+  belongs_to :offering, polymorphic: true
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
 end
