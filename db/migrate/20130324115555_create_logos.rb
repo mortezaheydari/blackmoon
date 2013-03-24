4cd2d7c69c842f17b76601d9f73814d3f8eaeae9
Shahin Hojabrian <shahin70000@gmail.com>
2013-03-24 18:26:00 +0300

    photo_id added to logo

--- db/migrate/20130324115555_create_logos.rb
+++ db/migrate/20130324115555_create_logos.rb
@@ -3,6 +3,7 @@ class CreateLogos < ActiveRecord::Migration
     create_table :logos do |t|
       t.integer :owner_id
       t.string :owner_type
+      t.integer :photo_id
 
       t.timestamps
     end
