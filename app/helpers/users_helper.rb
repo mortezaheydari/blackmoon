b6a9f8da0dd7459dc253974b75294f043f7a7c88
shahin70000 <shahin70000@gmail.com>
2013-03-25 09:51:03 +0300

    logo_for helper
    if user have a local icon, show that. else, show the gravatar.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,19 @@ module UsersHelper
 	def gravatar_for(user, options = {size: 50, gravatar_class: ""})
 		gravatar_id = Digest::MD5::hexdigest(user.account.email.downcase)
 		size = options[:size]
-                      gravatar_class = options[:gravatar_class]
+		gravatar_class = options[:gravatar_class]
 		gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 		image_tag(gravatar_url, als: user.name, class: "gravatar #{gravatar_class}")
 	end
+
+	def logo_for(user, options = {size: 50, gravatar_class: ""})
+		if user.logo.photo.nil?
+			gravatar_for(user, options)
+		else
+			size = options[:size]
+	    gravatar_class = options[:gravatar_class]
+			image_tag(user.logo.photo.image.url, als: user.name, class: "gravatar #{gravatar_class}")
+		end
+	end
+
 end
