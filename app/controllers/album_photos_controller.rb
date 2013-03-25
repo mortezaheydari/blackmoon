7e3f4dd679af26e486c3ee08be932c1802a2d557
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-03-25 10:39:55 +0300

    album_photos views created

--- app/controllers/album_photos_controller.rb
+++ app/controllers/album_photos_controller.rb
@@ -1,43 +1,41 @@
 class AlbumPhotosController < ApplicationController
 
   def create
-  	owner_type = params[:owner_type]
-  	owner_id = params[:owner_id]
+    owner_type = params[:owner_type]
+    owner_id = params[:owner_id]
     photo_title = params[:photo_title]
 
     if name_is_valid?(owner_type)
-		  @owner = owner_type.camelize.constantize.find_by_id(owner_id)    	
-			# checking photo upload premission
-			if owner_type == "user"
-				redirect_to @owner, notice: 'you don\'t have premission to upload photos to this page.' unless @owner == current_user
-			else
-				redirect_to @owner, notice: 'you don\'t have premission to upload photos to this page.' unless @owner.administrators.include? current_user
-			end
-
-			@album = @owner.album
-	    @photo = Photo.new(params[:photo])
-	    @album_photo = @photo.build_album_photo(album_id: @album.id)
-
-      if photo_title == ""
-        @album_photo.title = @photo.title
-      else
-        @album_photo.title = photo_title
-      end
-
-			if @photo.save
-		      if @album.save
-            respond_to do |format| 
-    	        format.html { redirect_to @owner, notice: 'Photo was successfully added.' }
-    	        format.js
-            end
-  	      else
-  	        redirect_to @owner, notice: 'error while uploading the photo.'
-  	      end			
+        @owner = owner_type.camelize.constantize.find_by_id(owner_id)
+        # checking photo upload premission
+        if owner_type == "user"
+            redirect_to @owner, notice: 'you don\'t have premission to upload photos to this page.' unless @owner == current_user
+        else
+            redirect_to @owner, notice: 'you don\'t have premission to upload photos to this page.' unless @owner.administrators.include? current_user
         end
-	    else
-        redirect_to @owner, notice: 'error while uploading the photo.'
-      end
 
+        @album = @owner.album
+        @photo = Photo.new(params[:photo])
+        @album_photo = @photo.build_album_photo(album_id: @album.id)
+
+        if photo_title == ""
+            @album_photo.title = @photo.title
+        else
+            @album_photo.title = photo_title
+        end
+
+        if @photo.save
+            if @album.save
+                respond_to do |format|
+                    format.html { redirect_to @owner, notice: 'Photo was successfully added.' }
+                    format.js
+                end
+            else
+                redirect_to @owner, notice: 'error while uploading the photo.'
+            end
+        else
+            redirect_to @owner, notice: 'error while uploading the photo.'
+        end
     else
         redirect_to root
     end
@@ -48,7 +46,7 @@ class AlbumPhotosController < ApplicationController
     owner_id = params[:owner_id]
 
     if name_is_valid?(owner_type)
-      @owner = owner_type.camelize.constantize.find_by_id(owner_id)     
+      @owner = owner_type.camelize.constantize.find_by_id(owner_id)
 
       # checking photo upload premission
       if owner_type == "user"
@@ -63,23 +61,23 @@ class AlbumPhotosController < ApplicationController
 
       if @photo.uses.count == 1
         if @photo.destroy
-          respond_to do |format|           
+          respond_to do |format|
             format.html { redirect_to @owner, notice: 'Photo was successfully removed.' }
-            format.js   
+            format.js
           end
         else
-          redirect_to @owner, notice: 'error while removing the photo.' 
+          redirect_to @owner, notice: 'error while removing the photo.'
         end
       else
-        if @album_photo.destory 
-          respond_to do |format|           
+        if @album_photo.destory
+          respond_to do |format|
             format.html { redirect_to @owner, notice: 'Photo was successfully removed.' }
-            format.js   
+            format.js
           end
         else
-          redirect_to @owner, notice: 'error while removing the photo.' 
-        end        
-      end 
+          redirect_to @owner, notice: 'error while removing the photo.'
+        end
+      end
 
     else
         redirect_to root
@@ -93,7 +91,7 @@ class AlbumPhotosController < ApplicationController
     photo_title = params[:photo_title]
 
     if name_is_valid?(owner_type)
-      @owner = owner_type.camelize.constantize.find_by_id(owner_id)     
+      @owner = owner_type.camelize.constantize.find_by_id(owner_id)
 
       # checking photo upload premission
       if owner_type == "user"
@@ -102,7 +100,7 @@ class AlbumPhotosController < ApplicationController
         redirect_to @owner, notice: 'you don\'t have premission to upload photos to this page.' unless @owner.administrators.include? current_user
       end
 
-      @album_photo = PhotoAlbum.find_by_id(params[:album_photo_id])     
+      @album_photo = PhotoAlbum.find_by_id(params[:album_photo_id])
       @photo = @album_photo.photo
 
       if photo_title == ""
@@ -110,15 +108,15 @@ class AlbumPhotosController < ApplicationController
       else
         @album_photo.title = photo_title
       end
-      
+
       if @album_photo.save
-        respond_to do |format| 
+        respond_to do |format|
           format.html { redirect_to @owner, notice: 'Photo was successfully added.' }
           format.js
         end
       else
         redirect_to @owner, notice: 'error while uploading the photo.'
-      end   
+      end
     else
         redirect_to root
     end
@@ -128,5 +126,5 @@ class AlbumPhotosController < ApplicationController
 
     def name_is_valid?(name)
       ["event","class","game", "user"].include? name
-    end	
+    end
 end
