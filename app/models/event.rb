38073d3d6f4fc300d1d5761821144cf124db8591
shahin70000 <shahin70000@gmail.com>
2013-03-26 14:23:15 +0300

    upload form Completed
    Users:
    - upload logo added
    - usercards photo added
    event:
    - upload logo added
    - controller updated
    -model updated
    Photo:
    -model updated

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,12 +8,18 @@ class Event < ActiveRecord::Base
   				 :time_to, :fee, :fee_type, :sport, :number_of_attendings, :team_participation
   before_save :default_values
 
+	after_create do |event|
+		event.create_album if event.album.nil?
+		event.create_logo if event.logo.nil?
+	end
+
 	make_flaggable :like
 
   has_one :album, as: :owner, :dependent => :destroy
   accepts_nested_attributes_for :album
 
   has_one :logo, as: :owner, :dependent => :destroy
+  accepts_nested_attributes_for :logo
 
   has_one :offering_creation, as: :offering, :dependent => :destroy
   accepts_nested_attributes_for :offering_creation
