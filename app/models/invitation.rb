92c247b903211af73f4a1a2c931d4e155098f0cd
shahin70000 <shahin70000@gmail.com>
2013-03-26 16:54:36 +0300

    accept_nested_attributes_for all

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,7 +2,10 @@ class Invitation < ActiveRecord::Base
   attr_accessible :invited_id, :invited_type, :inviter_id, :inviter_type, :message, :response_datetime, :state, :subject_id, :subject_type, :submission_datetime
 
   belongs_to :inviter, polymorphic: true
+  accepts_nested_attributes_for :inviter
   belongs_to :invited, polymorphic: true
+  accepts_nested_attributes_for :invited
   belongs_to :subject, polymorphic: true    
+  accepts_nested_attributes_for :subject  
 
 end
