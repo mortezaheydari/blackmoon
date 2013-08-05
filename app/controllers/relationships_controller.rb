fbff2b6896c86976718f7daeb5de0426295faa06
shahin70000 <shahin70000@gmail.com>
2013-08-05 11:43:09 +0300

    relationship activity monitoring
    - instead of creating activity on user, activity should be created on
    relationship itself
    - public_activity views added for user to avoid possible error

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,8 +3,8 @@ class RelationshipsController < ApplicationController
 
 	def create
 		@user = User.find(params[:relationship][:followed_id])
-		current_user.follow!(@user)
-		@user.create_activity :create, owner: current_user, recipient: @user
+		relationship = current_user.follow!(@user)
+		relationship.create_activity :create, owner: current_user, recipient: @user
 		respond_to do |format|
 			format.html { redirect_to @user }
 			format.js
@@ -12,9 +12,10 @@ class RelationshipsController < ApplicationController
 	end
 
 	def destroy
-		@user = Relationship.find(params[:id]).followed
+		relationship = Relationship.find(params[:id])
+		@user = relationship.followed
+		relationship.create_activity :destroy, owner: current_user, recipient: @user		
 		current_user.unfollow!(@user)
-		@user.create_activity :destroy, owner: current_user, recipient: @user		
 		respond_to do |format|
 			format.html { redirect_to @user }
 			format.js
