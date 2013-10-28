49cfc8fc7f3cc7a63636b30762306c70f53f086d
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-10-28 12:53:21 +0300

    Revert "Model Concerns - offerings"
    This reverts commit 31cfcdf2a778a33bdf702a327b933e65795cd450.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,110 +1,97 @@
 class Game < ActiveRecord::Base
 
-  include PublicActivity::Model
-  include Offerable
-  include Joinable
-  include Albumable  
-
+  include PublicActivity::Model    
   attr_accessible :category, :custom_address, :date_and_time, :description, :duration_type,
   	:fee, :fee_type, :location_type, :number_of_attendings, :sport,
   	:team_participation, :time_from, :time_to, :title, :tournament_id, :open_join
 
-## Joinable
-#  before_save :default_values
+  before_save :default_values
 
+  has_one :album, as: :owner, :dependent => :destroy
+  has_one :logo, as: :owner, :dependent => :destroy
 
-## Albumable
-#  after_create do |event|
-#      event.create_album if event.album.nil?
-#      event.create_logo if event.logo.nil?
-#  end
-#
-#  has_one :album, as: :owner, :dependent => :destroy
-#  accepts_nested_attributes_for :album
-#
-#  has_one :logo, as: :owner, :dependent => :destroy
-#  accepts_nested_attributes_for :logo
+    after_create do |event|
+        event.create_album if event.album.nil?
+        event.create_logo if event.logo.nil?
+    end
 
+	make_flaggable :like
 
-## Offerable
-#  make_flaggable :like
-#  has_one :offering_creation, as: :offering, :dependent => :destroy
-#  accepts_nested_attributes_for :offering_creation
-#
-#  has_many :offering_administrations, as: :offering, :dependent => :destroy
-#  accepts_nested_attributes_for :offering_administrations
+  has_one :album, as: :owner, :dependent => :destroy
+  accepts_nested_attributes_for :album
 
-## Joinable
-#  has_many :offering_individual_participations, as: :offering, :dependent => :destroy
-#  accepts_nested_attributes_for :offering_individual_participations
-#
-#  has_many :offering_team_participations, as: :offering, :dependent => :destroy
-#  accepts_nested_attributes_for :offering_team_participations
-#
-#	has_many :invitations, as: :subject, dependent: :destroy
-#	accepts_nested_attributes_for :invitations
-#
-#	has_one :happening_case, as: :happening, :dependent => :destroy
-#	accepts_nested_attributes_for :happening_case
-#
-#  has_many :join_requests_received, as: :receiver, class_name: "join_request"
-#  accepts_nested_attributes_for :join_requests_received
+  has_one :logo, as: :owner, :dependent => :destroy
+  accepts_nested_attributes_for :logo
 
-## Offerable
-#	def creator
-#		User.find_by_id(self.offering_creation.creator_id) unless self.offering_creation.nil?
-#	end
-#
-#	def administrators
-#		@admins = []
-#		self.offering_administrations.each do |admin|
-#			@admins << admin.administrator_id
-#		end
-#		User.find(@admins)
-#	end
-# 
-#  def location
-#    self.direct_location
-#  end
+  has_one :offering_creation, as: :offering, :dependent => :destroy
+  accepts_nested_attributes_for :offering_creation
 
-## Joinable
-#	def individual_participators
-#		@participators = []
-#		self.offering_individual_participations.each do |participation|
-#			@participators << participation.participator_id
-#		end
-#		User.find(@participators)
-#	end
-#
-#	def team_participators
-#		@participators = []
-#		self.offering_team_participations.each do |participation|
-#			@participators << participation.participator_id
-#		end
-#		Team.find(@participators)
-#	end
-#
-#  def inviteds
-#      @invited = []
-#      self.invitations.each do |invitation|
-#          @invited << invitation.invited
-#      end
-#      @invited
-#  end
-#
-#  def joineds
-#      @joineds = []
-#      self.individual_participators.each do |joined|
-#          @joineds << joined
-#      end
-#      self.team_participators.each do |joined|
-#          @joineds << joined
-#      end
-#      @joineds
-#  end
-#
-#  def default_values
-#    self.number_of_attendings ||= 0
-#  end
+  has_many :offering_administrations, as: :offering, :dependent => :destroy
+  accepts_nested_attributes_for :offering_administrations
 
+  has_many :offering_individual_participations, as: :offering, :dependent => :destroy
+  accepts_nested_attributes_for :offering_individual_participations
+
+  has_many :offering_team_participations, as: :offering, :dependent => :destroy
+  accepts_nested_attributes_for :offering_team_participations
+
+	has_many :invitations, as: :subject, dependent: :destroy
+	accepts_nested_attributes_for :invitations
+
+	has_one :happening_case, as: :happening, :dependent => :destroy
+	accepts_nested_attributes_for :happening_case
+
+  has_many :join_requests_received, as: :receiver, class_name: "join_request"
+  accepts_nested_attributes_for :join_requests_received
+
+	def creator
+		User.find_by_id(self.offering_creation.creator_id) unless self.offering_creation.nil?
+	end
+
+	def administrators
+		@admins = []
+		self.offering_administrations.each do |admin|
+			@admins << admin.administrator_id
+		end
+		User.find(@admins)
+	end
+
+	def individual_participators
+		@participators = []
+		self.offering_individual_participations.each do |participation|
+			@participators << participation.participator_id
+		end
+		User.find(@participators)
+	end
+
+	def team_participators
+		@participators = []
+		self.offering_team_participations.each do |participation|
+			@participators << participation.participator_id
+		end
+		Team.find(@participators)
+	end
+
+  def inviteds
+      @invited = []
+      self.invitations.each do |invitation|
+          @invited << invitation.invited
+      end
+      @invited
+  end
+
+  def joineds
+      @joineds = []
+      self.individual_participators.each do |joined|
+          @joineds << joined
+      end
+      self.team_participators.each do |joined|
+          @joineds << joined
+      end
+      @joineds
+  end
+
+  def default_values
+    self.number_of_attendings ||= 0
+  end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,46 +2,37 @@ class Venue < ActiveRecord::Base
   attr_accessible :descreption, :title, :location
 
   include PublicActivity::Model
-  include Offerable
-  include Albumable   
 
-## Albumable
-#  has_one :album, as: :owner, :dependent => :destroy
-#  accepts_nested_attributes_for :album
-#
-#  has_one :logo, as: :owner, :dependent => :destroy
-#  accepts_nested_attributes_for :logo 
-#
-#  after_create do |venue|
-#    venue.create_album if venue.album.nil?
-#    venue.create_logo if venue.logo.nil?
-#  end
+  after_create do |venue|
+    venue.create_album if venue.album.nil?
+    venue.create_logo if venue.logo.nil?
+  end
+
+  make_flaggable :like
 
   has_many :offering_sessions, as: :owner,:dependent => :destroy
   accepts_nested_attributes_for :offering_sessions
 
-## Offerable
-#  has_one :location, as: :owner, :dependent => :destroy
-#  accepts_nested_attributes_for :location
-#
-#  make_flaggable :like
-#  has_one :offering_creation, as: :offering, :dependent => :destroy
-#  accepts_nested_attributes_for :offering_creation
-#
-#  has_many :offering_administrations, as: :offering, :dependent => :destroy
-#  accepts_nested_attributes_for :offering_administrations
-#
-#  def creator
-#    User.find_by_id(self.offering_creation.creator_id) unless self.offering_creation.nil?
-#  end
-#
-#  def administrators
-#    @admins = []
-#    self.offering_administrations.each do |admin|
-#      @admins << admin.administrator_id
-#    end
-#    User.find(@admins)
-#  end
+  has_one :location, as: :owner, :dependent => :destroy
+  accepts_nested_attributes_for :location
+
+  has_one :album, as: :owner, :dependent => :destroy
+  accepts_nested_attributes_for :album
+
+  has_one :logo, as: :owner, :dependent => :destroy
+  accepts_nested_attributes_for :logo
+
+  has_one :offering_creation, as: :offering, :dependent => :destroy
+  accepts_nested_attributes_for :offering_creation
+
+  has_many :offering_administrations, as: :offering, :dependent => :destroy
+  accepts_nested_attributes_for :offering_administrations
+
+  has_many :invitations, as: :subject, dependent: :destroy
+  accepts_nested_attributes_for :invitations
+
+  has_many :join_requests_received, as: :receiver, class_name: "join_request"
+  accepts_nested_attributes_for :join_requests_received
 
   def offering_individual_participation
     @participations = []    
@@ -49,6 +40,37 @@ class Venue < ActiveRecord::Base
       @participations << session.individual_participation
     end
     @participations
+  end
+
+  def creator
+    User.find_by_id(self.offering_creation.creator_id) unless self.offering_creation.nil?
+  end
+
+  def administrators
+    @admins = []
+    self.offering_administrations.each do |admin|
+      @admins << admin.administrator_id
+    end
+    User.find(@admins)
+  end
+
+  def inviteds
+      @invited = []
+      self.invitations.each do |invitation|
+          @invited << invitation.invited
+      end
+      @invited
+  end
+
+  def joineds
+      @joineds = []
+      self.sessions.each do |joined|
+          @joineds << joined
+      end
+      self.team_participators.each do |joined|
+          @joineds << joined
+      end
+      @joineds
   end
 
 end
