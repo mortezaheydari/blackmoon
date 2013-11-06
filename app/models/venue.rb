110b7d3b263b8d261238e478a88657e7fc6ad313
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-11-06 09:55:32 +0300

    venue collective to collectives

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -12,10 +12,10 @@ class Venue < ActiveRecord::Base
   include Albumable
   	# album
   	# logo
-  	
+
   attr_accessible :descreption, :title, :location
 
   has_many :offering_sessions, as: :owner, :dependent => :destroy; accepts_nested_attributes_for :offering_sessions
-  has_many :collectives, as: :owner, :dependent => :destroy; accepts_nested_attributes_for :collective
+  has_many :collectives, as: :owner, :dependent => :destroy; accepts_nested_attributes_for :collectives
 
 end
