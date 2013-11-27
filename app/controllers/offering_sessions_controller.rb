479173c82945bf3d1d2af0ba0e26f17ef6d37d0c
shahin70000 <shahin70000@gmail.com>
2013-11-27 10:57:20 +0300

    fixing indentions

--- app/controllers/offering_sessions_controller.rb
+++ app/controllers/offering_sessions_controller.rb
@@ -23,7 +23,7 @@ class OfferingSessionsController < ApplicationController
 			# if creating mmultiple sessions, must automaticly create a collective to contain them. This allows user to delete those sessions easily in case of any mistakes by deleting the collectives.
 			case params[:offering_session][:collection_flag]
 			when "1"
-                                                assign_offering_session_owner
+												assign_offering_session_owner
 				@collective = create_collective(params[:offering_session][:title], @owner.class.to_s, @owner.id)
 				# @offering_session.owner = @collective.owner
 				@offering_session.collective = @collective
@@ -38,7 +38,7 @@ class OfferingSessionsController < ApplicationController
 			@offering_session.collective_id = @collective.id
 
 		when "new"
-                                    assign_offering_session_owner
+									assign_offering_session_owner
 			@collective = create_collective(params[:offering_session][:collective_title], @owner.class.to_s, @owner.id)
 			# @offering_session.owner = @collective.owner
 			@offering_session.collective = @collective
@@ -58,73 +58,68 @@ class OfferingSessionsController < ApplicationController
 			@repeat_every = params[:offering_session][:repeat_every].to_i
 
 			# repeat request is proper?
-			double_check(@owner) {
-				(["hour", "day", "month"].include? @repeat_duration) #&&
-				# @repeat_number < 26 &&
-				# @repeat_number > 0 &&
-				# @repeat_every < 26 &&
-				# @repeat_every > 0
-				 }
-                                    @attributes = @offering_session.attributes
-                                    ["id", "created_at", "updated_at"].each do |key|
-                                        @attributes.delete key
-                                    end
+				double_check(@owner) {
+			(["hour", "day", "month"].include? @repeat_duration) #&&
+			# @repeat_number < 26 &&
+			# @repeat_number > 0 &&
+			# @repeat_every < 26 &&
+			# @repeat_every > 0
+			 }
+
+			@attributes = @offering_session.attributes
+			["id", "created_at", "updated_at"].each do |key|
+				@attributes.delete key
+			end
+
 			# build multiple sessions, if "All Day" or "Range"
 			if params[:happening_case][:duration_type] == "All Day"
 				double_check(@owner) { !(params[:offering_session][:repeat_duration] == "hour") }
 				@repeat_number.times do |i|
-                                                            @the_offering_session = OfferingSession.new(@attributes)
-            				happening_case = @the_offering_session.build_happening_case(params[:happening_case])
-            				happening_case.date_and_time = (happening_case.date_and_time + (@repeat_every.send(@repeat_duration))*i)
-                                                            @the_offering_session.save
+					@the_offering_session = OfferingSession.new(@attributes)
+					happening_case = @the_offering_session.build_happening_case(params[:happening_case])
+					happening_case.date_and_time = (happening_case.date_and_time + (@repeat_every.send(@repeat_duration))*i)
+					@the_offering_session.save
 				end
-
 			elsif params[:happening_case][:duration_type] == "Range"
-
-                                            # @offering_sessions_list = Hash.new
-
-                                            @repeat_number.times do |i|
-                                                @the_offering_session = OfferingSession.new(@attributes)
-
-				happening_case = @the_offering_session.build_happening_case(params[:happening_case])
-				happening_case.time_from = (happening_case.time_from + (@repeat_every.send(@repeat_duration))*i)
-				happening_case.time_to = (happening_case.time_to + (@repeat_every.send(@repeat_duration))*i)
-                                                @the_offering_session.save
-
-			         end
-
+				# @offering_sessions_list = Hash.new
+				@repeat_number.times do |i|
+					@the_offering_session = OfferingSession.new(@attributes)
+					happening_case = @the_offering_session.build_happening_case(params[:happening_case])
+					happening_case.time_from = (happening_case.time_from + (@repeat_every.send(@repeat_duration))*i)
+					happening_case.time_to = (happening_case.time_to + (@repeat_every.send(@repeat_duration))*i)
+					@the_offering_session.save
+				end
 			else
 				double_check(@owner) { false }
 			end
 
 		else
 			@offering_session.build_happening_case(params[:happening_case])
-
-				double_check { @offering_session.save }
+			double_check { @offering_session.save }
 		end
 
-	    # return message.
-	    msg = "Session has been created."
-	    msg = "Sessions have been created." if params[:offering_session][:collection_flag] == "true"
+		# return message.
+		msg = "Session has been created."
+		msg = "Sessions have been created." if params[:offering_session][:collection_flag] == "true"
 
-                @grouped_happening_cases = grouped_happening_cases(@owner)
-                @grouped_sessions = replace_with_happening(@grouped_happening_cases)
-                @date = params[:date] ? Date.parse(params[:date]) : Date.today
-                @collectives = Collective.all
+		@grouped_happening_cases = grouped_happening_cases(@owner)
+		@grouped_sessions = replace_with_happening(@grouped_happening_cases)
+		@date = params[:date] ? Date.parse(params[:date]) : Date.today
+		@collectives = Collective.all
 
-	    respond_to do |format|
-	        format.html { redirect_to @owner, notice: msg }
-                    format.js
-	    end
+		respond_to do |format|
+			format.html { redirect_to @owner, notice: msg }
+			format.js
+		end
 
 	end
 
 	def update # offering_session
 		# find owner and offering_session
 
-                        # to be refactored
-                        # @owner = params[:offering_session][:owner_type].constantize.find(params[:offering_session][:owner_id])
-                        @owner = owner_if_reachable(params[:offering_session][:owner_type], params[:offering_session][:owner_id])
+		# to be refactored
+		# @owner = params[:offering_session][:owner_type].constantize.find(params[:offering_session][:owner_id])
+		@owner = owner_if_reachable(params[:offering_session][:owner_type], params[:offering_session][:owner_id])
 
 
 		@offering_session = OfferingSession.find(params[:id])
@@ -135,33 +130,32 @@ class OfferingSessionsController < ApplicationController
 
 			double_check(@owner, "This session has participators and can not be updated.") {
 		@offering_session.individual_participators.count == 0 }
-                        unless params[:offering_session][:collective_type] == "none" || @offering_session.collective_id == params[:offering_session][:collective_id]
-            		@collective = Collective.find params[:offering_session][:collective_id]
-            			double_check {
-                		@owner.collectives.include? @collective }
-                        end
 
-                        params[:offering_session].delete :collective_type
+		unless params[:offering_session][:collective_type] == "none" || @offering_session.collective_id == params[:offering_session][:collective_id]
+			@collective = Collective.find params[:offering_session][:collective_id]
+			double_check {
+			@owner.collectives.include? @collective }
+		end
 
-                        # to be used in case of changing collective_title
-                        collective_title = params[:offering_session].delete :collective_title
+		params[:offering_session].delete :collective_type
+
+		# to be used in case of changing collective_title
+		collective_title = params[:offering_session].delete :collective_title
 
 			double_check {
-    	@offering_session.update_attributes(params[:offering_session]) } # should become more secure in future.
+		@offering_session.update_attributes(params[:offering_session]) } # should become more secure in future.
 			double_check {
-    	@offering_session.happening_case.update_attributes(params[:happening_case]) } # should become more secure in future.
+		@offering_session.happening_case.update_attributes(params[:happening_case]) } # should become more secure in future.
 
+		@grouped_happening_cases = grouped_happening_cases(@owner)
+		@grouped_sessions = replace_with_happening(@grouped_happening_cases)
+				@collectives = Collective.all
+		@date = params[:date] ? Date.parse(params[:date]) : Date.today
 
-
-        @grouped_happening_cases = grouped_happening_cases(@owner)
-        @grouped_sessions = replace_with_happening(@grouped_happening_cases)
-                @collectives = Collective.all
-        @date = params[:date] ? Date.parse(params[:date]) : Date.today
-
-	    respond_to do |format|
-	        format.html { redirect_to @owner, notice: "Session has been updated." }
-	        format.js { render 'offering_sessions/update', :locals => { grouped_sessions: @grouped_sessions, owner: @owner } }
-	    end
+		respond_to do |format|
+			format.html { redirect_to @owner, notice: "Session has been updated." }
+			format.js { render 'offering_sessions/update', :locals => { grouped_sessions: @grouped_sessions, owner: @owner } }
+		end
 	end
 
 	def destroy # offering_session
@@ -176,10 +170,10 @@ class OfferingSessionsController < ApplicationController
 			double_check(@owner) {
 		@offering_session.destroy }
 
-	    respond_to do |format|
-	        format.html { redirect_to @owner, notice: "Session has been deleted." }
-	        format.js
-	    end
+		respond_to do |format|
+			format.html { redirect_to @owner, notice: "Session has been deleted." }
+			format.js
+		end
 	end
 
 	# secondary methods
@@ -196,10 +190,10 @@ class OfferingSessionsController < ApplicationController
 			double_check(@owner) {
 		@offering_session.save	}
 
-	    respond_to do |format|
-	        format.html { redirect_to @owner, notice: "Session has been released from collective." }
-	        format.js
-	    end
+		respond_to do |format|
+			format.html { redirect_to @owner, notice: "Session has been released from collective." }
+			format.js
+		end
 	end
 
 	def destroy_collective
@@ -217,7 +211,7 @@ class OfferingSessionsController < ApplicationController
 				double_check(@owner) {
 				offering_session.save }
 			end
-                                    @collective = Collective.find @collective.id
+									@collective = Collective.find @collective.id
 				double_check(@owner) {
 			@collective.destroy }
 			@msg = "Collective has been destroyed while keeping its sessions."
@@ -231,28 +225,28 @@ class OfferingSessionsController < ApplicationController
 				end
 			end
 
-                                    @collective = Collective.find @collective.id
+			@collective = Collective.find @collective.id
 				double_check(@owner) {
 			@collective.destroy }
 			@msg = "Collective has been destroyed alongside its sessions."
 		end
 
-	    respond_to do |format|
-	        format.html { redirect_to @owner, notice: "Collective has been destroyed." }
-	        format.js
-	    end
+		respond_to do |format|
+			format.html { redirect_to @owner, notice: "Collective has been destroyed." }
+			format.js
+		end
 
 	end
 
 	private
 
-                        def grouped_happening_cases(this)
-                            session_id_list = []
-                            this.offering_sessions.each do |os|
-                                session_id_list << os.id
-                            end
-                            sorted_happening_cases = HappeningCase.where(happening_type: "OfferingSession", happening_id: session_id_list).group_by(&:date_and_time)
-                        end
+		def grouped_happening_cases(this)
+			session_id_list = []
+			this.offering_sessions.each do |os|
+				session_id_list << os.id
+			end
+			sorted_happening_cases = HappeningCase.where(happening_type: "OfferingSession", happening_id: session_id_list).group_by(&:date_and_time)
+		end
 
 		def user_must_be_admin?
 			@event = Event.find(params[:id])
@@ -280,14 +274,14 @@ class OfferingSessionsController < ApplicationController
 			@offering_session.owner = @owner
 		end
 
-                        def replace_with_happening(grouped_happening_cases)
-                            grouped_sessions = Hash.new
-                            grouped_happening_cases.each do |key, value|
-                                grouped_sessions[key.to_date] = []
-                                value.each do |happening_case|
-                                    grouped_sessions[key.to_date] << happening_case.happening
-                                end
-                            end
-                            grouped_sessions
-                        end
+		def replace_with_happening(grouped_happening_cases)
+			grouped_sessions = Hash.new
+			grouped_happening_cases.each do |key, value|
+				grouped_sessions[key.to_date] = []
+				value.each do |happening_case|
+					grouped_sessions[key.to_date] << happening_case.happening
+				end
+			end
+			grouped_sessions
+		end
 end
