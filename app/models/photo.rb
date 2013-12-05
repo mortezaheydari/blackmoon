59c8b636c4ce0a18f057962c69df13f659dc0a08
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-12-05 15:19:17 +0300

    photo model fixed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,13 @@
 class Photo < ActiveRecord::Base
   attr_accessible :title, :image
 
+            has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>", :large => "1024x768>", :small => "50x50#", :user_profile => "156x156#", :offering_card => "200x160#" }
+
 	validates_attachment :image,
 		:presence => true,
 		:content_type => { :content_type =>  ['image/png', 'image/jpg', 'image/jpeg'] },
 		:size => { :in => 0..10000.kilobytes }
 
-  has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>", :large => "1024x768>", :small => "50x50#", :user_profile => "156x156#", :offering_card => "200x160#" }
 
 	has_many :logos
 	accepts_nested_attributes_for :logos
