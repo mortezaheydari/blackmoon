d957faf4e1b0877ccef9d63e4c9201c933c2a6c8
shahin70000 <shahin70000@gmail.com>
2013-12-15 17:10:13 +0300

    location model updated

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,16 +1,34 @@
 class Location < ActiveRecord::Base
 	attr_accessible :city, :custom_address, :custom_address_use, :gmap_use, :latitude, :longitude, :owner_id, :owner_type, :title, :gmaps, :parent_id
+	attr_accessor :parent, :parent_location, :parent_venue
 
 	belongs_to :owner, polymorphic: true
 
-	belongs_to :parent; accepts_nested_attributes_for :parent
-	has_many :child_locations, as: :parent; accepts_nested_attributes_for :child_locations
+	belongs_to :parent, class_name: "Location"; accepts_nested_attributes_for :parent
+	has_many :child_locations, as: :parent, class_name: "Location"; accepts_nested_attributes_for :child_locations
 	before_destroy :detach_childs
 
 	acts_as_gmappable
 
 	def gmaps4rails_address
 		self.custom_address.to_s
+	end
+
+	def parent_location
+		if !parent_id.nil?
+			location = Location.find(parent_id)
+		else
+			nil
+		end
+	end
+
+	def parent_venue
+		if !parent_id.nil?
+			location = Location.find(parent_id)
+			location.owner
+		else
+			nil
+		end
 	end
 
 	private
