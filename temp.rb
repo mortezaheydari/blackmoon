f11c75c294504291fdd71c01cbfdbf741b4652d1
shahin70000 <shahin70000@gmail.com>
2013-12-22 13:22:04 +0300

    more files removed

--- temp.rb
+++
@@ -1,7 +0,0 @@
-<%= link_to "<div class='joinButton'></div>", 
-					offering_individual_participations_create_path(offering_type: "event", 
-									offering_id: @event.id, joining_user: current_user), method: :post %>
-
-<%= link_to "<div class='joinButton'></div>', 
-					offering_individual_participations_create_path(offering_type: "event", 
-									offering_id: @event.id, joining_user: current_user), method: :post %>
