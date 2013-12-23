14a666d33d6c9c91242320d4a8bd0fdb102f98bf
shahin70000 <shahin70000@gmail.com>
2013-12-23 17:32:40 +0300

    act_membership refactored

--- app/controllers/act_memberships_controller.rb
+++ app/controllers/act_memberships_controller.rb
@@ -10,23 +10,28 @@ class ActMembershipsController < ApplicationController
 		user = User.find_by_id(params[:joining_user])
 		act_id = params[:offering_id]
 
-		if !name_is_valid?(user, act_type); raise Errors::FlowError.new; end
+		if !name_is_valid?(user, act_type); raise Errors::FlowError.new(send("#{act_type}_path", offering_id), "Permission denied."); end
 
 		acts_membership = user.send("#{act_type}s_membership")
 		joining_act = act_type.camelize.constantize.find_by_id(act_id)
 
+		unless joining_act; raise Errors::FlowError.new; end
+
 		# gender restriction
 		if ["male", "female"].include? joining_act.gender
 			unless user.gender == joining_act.gender; raise Errors::FlowError.new(root_path, "This action is not possible because of gender restriction."); end
-		end		
+		end	
 			
 		number_of_attendings = joining_act.number_of_attendings
 		if acts_membership.count < number_of_attendings or number_of_attendings == 0
 			acts_membership << joining_act
+		else
+			raise Errors::FlowError.new(send("#{act_type}_path", offering_id), "Permission denied.")
 		end
 		@offering = joining_act
 		@members = joining_act.members
 		joining_act.create_activity :create, owner: user
+
 		respond_to do |format|
 			format.html { redirect_to joining_act }
 			format.js
@@ -38,9 +43,12 @@ class ActMembershipsController < ApplicationController
 		user = User.find_by_id(params[:leaving_user])
 		act_id = params[:offering_id]
 
-		if !name_is_valid?(user, act_type); raise Errors::FlowError.new; end
+		if !name_is_valid?(user, act_type); raise Errors::FlowError.new(send("#{act_type}_path", offering_id), "Permission denied."); end
 
 		acts_memberships = user.act_memberships.where(act_type: act_type, act_id: act_id)
+
+		unless acts_memberships; raise Errors::FlowError.new; end
+		
 		acts_memberships.each.destroy unless acts_memberships == []
 
 		leaving_act = act.camelize.constantize.find_by_id(act_id)
@@ -56,7 +64,7 @@ class ActMembershipsController < ApplicationController
 	private
 		# checks if offering name is valid for team
 		# note: this function is controller specific
-		def name_is_valid?(user, name)
-			user.respond_to? "#{name}s_membership" and ["team", "sponsor", "organization"].include? name
+		def name_is_valid?(user, this)
+			user.respond_to? "#{name}s_membership" and this_is_act?(this)
 		end
 end
