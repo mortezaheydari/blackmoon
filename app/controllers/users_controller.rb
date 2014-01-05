95dbbb1031cf968e6822a824fc6515c73daacefb
shahin70000 <shahin70000@gmail.com>
2014-01-05 16:09:29 +0300

    debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,7 +80,7 @@ class UsersController < ApplicationController
 	    def safe_param
 			this = Hash.new
 			Rails.logger.warn '-'*40
-			Rails.logger.warn params
+			Rails.logger.warn params.inspect
 			this[:name] = params[:user][:name] unless params[:user][:name].nil?
 			this[:gender] = params[:user][:gender] unless params[:user][:gender].nil?
 			this[:profile_attributes][:first_name] = params[:user][:profile_attributes][:first_name] unless params[:user][:profile_attributes][:first_name].nil?			
